adfd74663b875c39df846df53975748bbdd20e19
suyashmisra <suyash.misra@quovantis.com>
2021-02-12 15:11:42 +0530

    added handlebar

--- src/app.js
+++ src/app.js
@@ -6,7 +6,31 @@ const app = express(); //creates an express application and returns it
 
 const directoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs"); //lets express know that hbs has been setup as the view engine
+//hbs is a package that allows us to use handlebars in Express
+
 app.use(express.static(directoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Welcome to homepage",
+    name: "Suyash Misra",
+  }); //renders the template, second argument sends dynamic content to the template in first argument
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Suyash Misra",
+  }); //renders the template, second argument sends dynamic content to the template in first argument
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    message: "Here to help",
+  }); //renders the template, second argument sends dynamic content to the template in first argument
+});
 
 app.get("/weather", (req, res) => {
   res.send({
