24c6e73e79c277f06ff474b4d988a2e0c3776ea0
suyashmisra <suyash.misra@quovantis.com>
2021-02-12 18:28:59 +0530

    added partials

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const path = require("path"); //core module for path handling
+const hbs = require("hbs");
 
 const express = require("express"); //returns a function
 
@@ -6,10 +7,18 @@ const app = express(); //creates an express application and returns it
 
 const directoryPath = path.join(__dirname, "../public");
 
+const viewPath = path.join(__dirname, "../templates/views");
+
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+hbs.registerPartials(partialsPath);
+
 app.set("view engine", "hbs"); //lets express know that hbs has been setup as the view engine
 //hbs is a package that allows us to use handlebars in Express
 
-app.use(express.static(directoryPath));
+app.set("views", viewPath); //sets express to fetch hbs templates from viewPath directory
+
+app.use(express.static(directoryPath)); //directory to load static webpages
 
 app.get("", (req, res) => {
   res.render("index", {
@@ -22,14 +31,15 @@ app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
     name: "Suyash Misra",
-  }); //renders the template, second argument sends dynamic content to the template in first argument
+  });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
     title: "Help page",
     message: "Here to help",
-  }); //renders the template, second argument sends dynamic content to the template in first argument
+    name: "Suyash Misra",
+  });
 });
 
 app.get("/weather", (req, res) => {
